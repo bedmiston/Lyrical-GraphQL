e9f7ede61f444e49ff375411f3347f46bfea61db
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-14 14:56:15 -0600

    Add ability to add a lyric to a song

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: "" };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        songId: this.props.songId,
+        content: this.state.content
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+      </form>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
