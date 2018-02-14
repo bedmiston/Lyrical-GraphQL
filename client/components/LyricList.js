2d0b5f1a277fbdbb5267298d64f29c6e58673f11
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-14 15:38:15 -0600

    Add a like button and handler

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class LryicList extends Component {
+  likeHandler(id) {
+    console.log(id);
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <i className="material-icons" onClick={() => this.likeHandler(id)}>
+            thumb_up
+          </i>
         </li>
       );
     });
